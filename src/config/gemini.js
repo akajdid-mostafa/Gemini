b8a8d236441ee438f99ca54cd52f351547a953c9
akajdid-mostafa <125519651+akajdid-mostafa@users.noreply.github.com>
2025-02-12 15:34:01 +0100

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-// Access your API key as an environment variable (see "Set up your API key" above)
+// Access your API key as as as as as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
